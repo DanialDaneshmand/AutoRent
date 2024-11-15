54ecc74ae84558a081c90b46ce53825d01567bb7
danial daneshmand <daniald11111377@gmail.com>
2024-11-14 23:50:16 -0800

    my payment fixed

--- src/pages/MyPayment.jsx
+++ src/pages/MyPayment.jsx
@@ -7,15 +7,13 @@ function MyPayment() {
         <p className=" text-gray-600 text-lg"> پرداخت های من</p>
       </div>
       <div className=" py-4">
-        <div className=" border my-4 rounded-lg p-4 flex flex-col sm:flex-row justify-between">
+      <div className=" border my-4 rounded-lg p-4 flex flex-col sm:flex-row justify-between">
           <div className=" flex flex-col items-start sm:flex-row gap-x-4">
-            <div className=" w-full flex justify-center">
             <img
-              src="/images/benz-g-class.png"
+              src="/images/toyota-land.png"
               className=" w-64 h-64 rounded-lg border"
               alt=""
             />
-            </div>
             <div className=" my-4 sm:my-0 flex flex-col h-full  justify-center items-center gap-y-4">
               <span className="text-gray-600 font-bold">بنز g class</span>
               <span className=" text-sm">
@@ -44,6 +42,7 @@ function MyPayment() {
             </span>
           </div>
         </div>
+        
         <div className=" border my-4 rounded-lg p-4 flex flex-col sm:flex-row justify-between">
           <div className=" flex flex-col items-start sm:flex-row gap-x-4">
             <img
@@ -64,7 +63,7 @@ function MyPayment() {
             </div>
           </div>
           <div className=" flex flex-col gap-y-2 text-sm justify-center">
-            <span className=" font-bold text-red-600">پرداخت موفق</span>
+            <span className=" font-bold text-red-600">پرداخت نا موفق</span>
             <span>
               <span className=" text-gray-400"> شناسه پرداخت : </span>
               <span className=" text-gray-600">25621385482475</span>
